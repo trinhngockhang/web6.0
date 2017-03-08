a3d5d2f0bdc18a702f9a9e31a2c474bf2d01f25d
Khang <trinhngockhang1503@gmail.com>
2017-03-08 20:32:39 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var Nakama = {};
+Nakama.configs = {};
+
+window.onload = function(){
+  Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
+    {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    }, false, false
+  );
+}
+
+// preparations before game starts
+var preload = function(){
+  Nakama.game.scale.minWidth = 320;
+  Nakama.game.scale.minHeight = 480;
+  Nakama.game.scale.maxWidth = 640;
+  Nakama.game.scale.maxHeight = 960;
+  Nakama.game.scale.pageAlignHorizontally = true;
+  Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+  Nakama.game.time.advancedTiming = true;
+
+  Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
+  Nakama.game.load.image('background', 'Assets/Map1.png');
+}
+
+// initialize the game
+var create = function(){
+  Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
+  Nakama.keyboard = Nakama.game.input.keyboard;
+}
+
+// update game state each frame
+var update = function(){
+
+}
+
+// before camera render (mostly for debug)
+var render = function(){}
